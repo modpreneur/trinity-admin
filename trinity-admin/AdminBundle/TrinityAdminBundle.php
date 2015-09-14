0275501a3d6ff965039e55852e16b37cea35ca5c
Tomáš Jančar <jan02@modpreneur.com>
2015-09-14 12:56:07 +0200

    init

--- trinity-admin/AdminBundle/TrinityAdminBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Trinity\AdminBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class TrinityAdminBundle extends Bundle
-{
-}
--- trinity-admin/README.md
+++
@@ -1,8 +0,0 @@
-# README
-
-Trinity Admin Bundle
-====================
-
-
-
-
