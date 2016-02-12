d3e41414f7202f6fa9c13d424cf7517e852911aa
Tomáš Jančar <jan02@modpreneur.com>
2016-02-12 10:26:56 +0100

    fixes

--- AdminBundle/Twig/ControllerActionExtension.php
+++ AdminBundle/Twig/ControllerActionExtension.php
@@ -44,8 +44,8 @@ class ControllerActionExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            'get_controller_name' => new \Twig_Function_Method($this, 'getControllerName'),
-            'get_action_name' => new \Twig_Function_Method($this, 'getActionName'),
+            'get_controller_name' => new \Twig_SimpleFunction($this, 'getControllerName'),
+            'get_action_name' => new \Twig_SimpleFunction($this, 'getActionName'),
         );
     }
 
