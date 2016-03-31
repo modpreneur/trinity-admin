4ea1bf11c6d53b5b634834aa8264f3e2c287c2d5
Bureš <richardbures@email.cz>
2016-03-31 10:50:47 +0200

    added ColorType

---
+++ AdminBundle/Form/Color/ColorType.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rockuo
+ * Date: 31.3.16
+ * Time: 10:40
+ */
+
+namespace Trinity\AdminBundle\Form\Color;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+
+/**
+ * Class ColorType
+ * @package Trinity\AdminBundle\Form\Color
+ */
+class ColorType extends AbstractType
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return TextType::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'color';
+    }
+}
