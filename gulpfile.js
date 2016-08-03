3e37de64988265d8f6fb2dd13fad20a55423dc51
Tomas Jancar <jancar@modpreneur.com>
2016-08-03 13:37:59 +0200

    gulp :)

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,50 @@
-var gulp = require('gulp');
-
-var less  = require('gulp-less');
-var path  = require('path');
-var watch = require('gulp-watch');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var less        = require('gulp-less');
+var path        = require('path');
 
 var LessAutoprefix = require('less-plugin-autoprefix');
-var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
-
-gulp.task('admin', function() {
-
-    return watch('./AdminBundle/Resources/Public/less/admin.less')
-        .pipe(less({
-            paths: [ path.join(__dirname, 'less', 'includes') ],
-            compress: true,
-            plugins: [autoprefix]
-        }))
-        .pipe(gulp.dest('./AdminBundle/Resources/Public/css/'));
-});
-
-gulp.task('theme-brown', function() {
-
-    return watch('./AdminBundle/Resources/Public/theme/Brown/theme.less')
-        .pipe(less({
-            paths: [ path.join(__dirname, 'less', 'includes') ],
-            compress: true,
-            plugins: [autoprefix]
-        }))
-        .pipe(gulp.dest('./AdminBundle/Resources/Public/theme/Brown'));
-});
+var autoprefix     = new LessAutoprefix({ browsers: ['last 2 versions'] });
 
 
 gulp.task('theme-dark-blue', function() {
 
-    return watch('./AdminBundle/Resources/Public/theme/DarkBlue/theme.less')
+    return gulp.src('./AdminBundle/Resources/Public/theme/DarkBlue.less')
         .pipe(less({
             paths: [ path.join(__dirname, 'less', 'includes') ],
             compress: true,
             plugins: [autoprefix]
         }))
-        .pipe(gulp.dest('./AdminBundle/Resources/Public/theme/DarkBlue'));
+        .pipe(gulp.dest('./AdminBundle/Resources/Public/theme'));
 });
 
 
-gulp.task('default', ['admin', 'theme-brown', 'theme-dark-blue']);
+gulp.task('theme-brown', function() {
+
+    return gulp.src('./AdminBundle/Resources/Public/theme/Brown.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ],
+            compress: true,
+            plugins: [autoprefix]
+        }))
+        .pipe(gulp.dest('./AdminBundle/Resources/Public/theme'));
+});
+
+
+gulp.task('theme-watch', ['theme-dark-blue', 'theme-brown'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+
+gulp.task('serv', ['theme-dark-blue', 'theme-brown'], function () {
+    browserSync.init({
+        proxy: "necktie.docker/app_dev.php/admin"
+    });
+
+    gulp.watch("./AdminBundle/Resources/Public/theme/DarkBlue.less", ['theme-watch']);
+    gulp.watch("./AdminBundle/Resources/Public/theme/Brown.less", ['theme-watch']);
+});
+
+
+gulp.task('default', ['serv', 'theme-brown', 'theme-dark-blue']);
