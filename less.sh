da6a786effab5f0af8bcf94d6d53dbe93fbd4c7d
Marek Omasta <m.omasta@outlook.com>
2016-12-15 17:11:14 +0100

    Small fixes for Necktie

--- less.sh
+++ less.sh
@@ -3,3 +3,4 @@
 node_modules/less/bin/lessc --clean-css AdminBundle/Resources/Public/less/admin.less AdminBundle/Resources/Public/css/admin.css
 node_modules/less/bin/lessc --clean-css AdminBundle/Resources/Public/less/theme/brown.less AdminBundle/Resources/Public/css/theme/brown.css
 node_modules/less/bin/lessc --clean-css AdminBundle/Resources/Public/less/theme/dark-blue.less AdminBundle/Resources/Public/css/theme/dark-blue.css
+node_modules/less/bin/lessc --clean-css AdminBundle/Resources/Public/less/error.less AdminBundle/Resources/Public/css/error.css
